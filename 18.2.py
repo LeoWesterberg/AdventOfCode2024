90970217e0d5f456157404d265cdf864e65324cc
Leo Westerberg <lewe@netlight.com>
2024-12-18 21:07:39 +0100

    added day 18 solutions

---
+++ 18.2.py
@@ -0,0 +1,42 @@
+import heapq
+
+def readinput(filename):
+    count = 0
+    mem = [["."] * 71 for i in range(71)]
+    rest = []
+    with open(filename) as file:
+        for line in file:
+            x, y = tuple(line.split(","))
+            if count < 1024:
+                mem[int(y)][int(x)] = "#"
+                count += 1
+            else:
+                rest.append((int(x),int(y)))
+    return mem, rest
+
+def djikstras(grid, x, y, target):
+    visited = set()
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    queue = [] 
+    cost = None
+    heapq.heappush(queue, (0, (x, y)))
+    while(len(queue) > 0):
+        cost, coord = heapq.heappop(queue)
+        if coord == target:
+            return True
+        if coord not in visited:
+            x, y = coord
+            visited.add(coord)
+            for new_dir in dirs:
+                dir_pos_x, dir_pos_y = x + new_dir[0], y + new_dir[1]
+                if 0 <= dir_pos_y < len(grid) and 0 <= dir_pos_x < len(grid[0]):
+                    if grid[dir_pos_y][dir_pos_x] != "#":
+                        heapq.heappush(queue, (cost + 1, (dir_pos_x, dir_pos_y)))
+    return False
+
+grid, rest = readinput("inputs/18.txt")
+for (x, y) in rest:
+    grid[y][x] = "#"
+    if not djikstras(grid, 0, 0, (70, 70)):
+        print(f"{x},{y}",)
+        break
